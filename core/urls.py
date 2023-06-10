50d2055ed2dcbb5cdd0a2e1035b8a04bff9709e2
Erfan Esmaeeli <erfan.es00749@gmail.com>
2023-06-10 18:26:36 +0330

    develop accounts

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Accounts Urls 
-    # path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     
     # Main Urls
     path('', include('main.urls', namespace='main')),
@@ -15,3 +17,9 @@ urlpatterns = [
     # Demo Urls
     path('demo/', include('demo.urls', namespace='demo')),
 ]
+
+
+# Development
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
